ff6c35210930b2315600330c0601340af69001dd
HeartDV <deveraheart8@gmail.com>
2024-06-08 09:56:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ btnLike4.addEventListener("click", countLike4)
 
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
-const commentbox= document.getElementById("commentbox")
+const commentbox = document.getElementById("commentbox")
 function submitComment(){
   commentbox.textContent += comment.value.toString() + "\n"
 }
