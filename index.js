f8167f1fa7936b5b7bf4fa4a7da6388b9d60f13f
HeartDV <deveraheart8@gmail.com>
2024-06-08 10:35:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const btnLike1 = document.getElementById("btnLike1")
 const count1 = document.getElementById("count1")
 
 function countLike1() {
-  let likeCounts = parseInt(count1.value) + 1
-  count1.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count1.innerText) + 1
+  count1.innerText = likeCounts.toString()
 }
 
 btnLike1.addEventListener("click", countLike1)
@@ -13,8 +13,8 @@ const btnLike2 = document.getElementById("btnLike2")
 const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count2.value) + 1
-  count2.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count2.innerText) + 1
+  count2.innerText = likeCounts.toString()
 }
 
 btnLike2.addEventListener("click", countLike2)
@@ -24,8 +24,8 @@ const btnLike3 = document.getElementById("btnLike3")
 const count3 = document.getElementById("count3")
 
 function countLike3() {
-  let likeCounts = parseInt(count3.value) + 1
-  count3.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count3.innerText) + 1
+  count3.innerText = likeCounts.toString()
 }
 
 btnLike3.addEventListener("click", countLike3)
@@ -35,8 +35,8 @@ const btnLike4 = document.getElementById("btnLike4")
 const count4 = document.getElementById("count4")
 
 function countLike4() {
-  let likeCounts = parseInt(count4.value) + 1
-  count4.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count4.innerText) + 1
+  count4.innerText = likeCounts.toString()
 }
 
 btnLike4.addEventListener("click", countLike4)
