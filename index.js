35912a4717240652f31f8f4845bcebdb9c6ca971
HeartDV <deveraheart8@gmail.com>
2024-06-08 09:42:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,11 @@ function countLike4() {
 
 btnLike4.addEventListener("click", countLike4)
 
+
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox= document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}
+submit.addEventListener("click",submitComment) 
